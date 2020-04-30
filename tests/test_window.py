6fc11ed05afb60e57c6f3a880f0aac4ab44a4df4
idle sign <idlesign@yandex.ru>
2020-04-30 18:22:03 +0700

    Travis troubleshoot.

--- tests/test_window.py
+++ tests/test_window.py
@@ -1,12 +1,8 @@
-import pytest
-
 from ocvproto.toolbox import Window
 
 
-@pytest.mark.skip(reason='UI backend may be unavailable')
 def test_window():
     win = Window()
     win.create(autosize=False)
     win.position(x=10, y=20)
     win.resize(width=50, height=50)
-
